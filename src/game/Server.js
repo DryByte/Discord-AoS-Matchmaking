999ea58c6a3cdf4baad24040cd712d2197944213
DryByte <93609026+DryByte@users.noreply.github.com>
2022-11-12 16:27:35 -0300

    Create server folder and copy piqueserver files to it

--- src/game/Server.js
+++ src/game/Server.js
@@ -1,4 +1,6 @@
 const Proxy = require("./Proxy.js");
+const { spawn } = require("child_process");
+const { accessSync, mkdirSync, constants } = require("fs");
 
 class Server {
     constructor(name, port_range, proxy_port) {
@@ -16,11 +18,33 @@ class Server {
     }
 
     startProxy() {
-        this.proxy = new Proxy(this.proxy_port);
+        //this.proxy = new Proxy(this.proxy_port);
+    }
+
+    createServerFolder() {
+        try {
+            accessSync(`./servers/${this.identifier}`, constants.R_OK | constants.W_OK);
+            return true;
+
+        } catch(e) {
+            if (e.code == "ENOENT"){
+                let serverPath = `${__dirname}/../../servers/${this.identifier}`;
+
+                mkdirSync(`./servers/${this.identifier}`);
+                spawn(`piqueserver`, ["--copy-config", "-d",serverPath]);
+                return true;
+            }
+
+            return false;
+        }
     }
 
     startServer() {
-
+        let stats = this.createServerFolder();
+        if (!stats) {
+            console.log(`Can't start server ${this.identifier}.`);
+            return false;
+        }
     }
 }
 
